427e1e84a8d4cada54d85bb1000bee4ce2add6de
PRAN <prantokumar.net@gmail.com>
2024-08-23 14:29:00 +0600

    .Net added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _NET;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("C# running Ok.");
+    }
+}
