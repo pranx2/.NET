0b7906689c1780132ca2fb438cad66c54257060b
PRAN <prantokumar.net@gmail.com>
2024-08-24 14:12:59 +0600

    ternary added

--- Ternary.cs
+++ Ternary.cs
@@ -1,5 +1,5 @@
-// Console.WriteLine($"Ternary Operator!");
-//  int result = 5 > 3 ? 5 : 3 ; // if num1 > num2 then print num1 otherwise print num2;
-//  Console.WriteLine(result);
-//  int result2 = 3 > 5 ? 5 : 3;
-//  Console.WriteLine(result2);
+Console.WriteLine($"Ternary Operator!");
+ int result = 5 > 3 ? 5 : 3 ; // if num1 > num2 then print num1 otherwise print num2;
+ Console.WriteLine(result);
+ int result2 = 3 > 5 ? 5 : 3;
+ Console.WriteLine(result2);
