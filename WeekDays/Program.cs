02a35529adf6c82dcf9f30f6805e82e312079b7f
PRAN <prantokumar.net@gmail.com>
2024-08-27 23:56:33 +0600

    updated weekDays

---
+++ WeekDays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace weekDays{
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter week days : ");
+        string weekDays = ReadLine();
+        switch (weekDays.ToUpper()){
+          case "SATURDAY":
+            Console.WriteLine("Saturday");
+            break;
+          case "SUNDAY":
+            Console.WriteLine("Sunday");
+            break;
+          case "MONDAY":
+            Console.WriteLine("Monday");
+            break;
+          case "TUESDAY":
+            Console.WriteLine("Tuesday");
+            break;
+          case "WEDNESDAY":
+           Console.WriteLine("Wednesday");
+           break;
+          case "THURSDAY":
+           Console.WriteLine("Thursday");
+           break;
+        }
+    }
+}
+}
