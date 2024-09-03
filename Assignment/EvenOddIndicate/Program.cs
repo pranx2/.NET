6b4a7c3e0fd1d53a69b2d06635c18d7411835ec2
Pranto Kumar <prantokumar.net@gmail.com>
2024-09-03 22:32:49 +0600

    Even Odd Indicate

---
+++ Assignment/EvenOddIndicate/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EvenOddIndicate;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Even Odd Indicator\n");
+        for(int i = 1; i <= 10; ++i){
+            if( i % 2 == 0 )Console.WriteLine($"Even {i}");
+            else Console.WriteLine($"Odd {i}");
+        }
+    }
+}
