08c6e3d79b40a7474130ed3582bbf1f2c5181399
Pranto Kumar <prantokumar.net@gmail.com>
2024-09-03 22:50:24 +0600

    Array Odd Even Checked and Sum

--- Assignment/ArraySum/Program.cs
+++ Assignment/ArraySum/Program.cs
@@ -7,14 +7,17 @@ class Program
         Console.WriteLine("Array Sum!\nEnter the array size :");
         int size = Convert.ToInt32(Console.ReadLine());
         int[] arr = new int[size];
+        int evenSum = 0, OddSum = 0;
         for(int i = 0; i < size; i++){
             Console.WriteLine($"Enter the array {i} value : ");
             arr[i] = Convert.ToInt32(Console.ReadLine());
         }
         for(int i = 0; i < size;  i++){
-            if( arr[i] % 2 == 0 )Console.WriteLine($"Array {i} value : {arr[i]}  is even");
-            else Console.WriteLine($"Array {i} {arr[i]} is odd");
+            if( arr[i] % 2 == 0 ) evenSum += arr[i];
+            else OddSum += arr[i];
         }
+        Console.WriteLine($"Total Even Sum : {evenSum}");
+        Console.WriteLine($"Total Odd Sum : {OddSum}");
 
     }
 }
