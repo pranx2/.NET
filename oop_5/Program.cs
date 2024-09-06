da186053eb9406fcde31615e62b9295b51f1d94e
Pranto Kumar <prantokumar.net@gmail.com>
2024-09-06 19:42:37 +0600

    Readonly and This using   in class

---
+++ oop_5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace oop_5;
+//this and Readonly modifier;
+public class student{
+    //access  to write by Declaration and Constructor 
+    public readonly int age; // only read not access to write;
+    // public readonly int age = 40; //using by declaration;
+    public student(int age){ // using by constructor;
+        this.age = age; //this means class current object;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        student s1 = new student(40); // using  by constructor;
+
+        // s1.age = 30; // using by declaration;
+    Console.WriteLine($"age is  : {s1.age}");
+    }
+}
