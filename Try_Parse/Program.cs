2215893528881504263aa4d11d6abda10fc30d81
Pranto Kumar <prantokumar.net@gmail.com>
2024-09-07 20:25:36 +0600

    Try parse

---
+++ Try_Parse/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Try_Parse;
+//Try parse is a bool type which takes the user input and return the true/false 
+//is user input is incorrect then its show false also the vice-versa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Try Parse!");
+        string input = (Console.ReadLine()) ?? "";
+        bool x = int.TryParse(input, out int output);//checking the value is correct with data types or not;
+        Console.WriteLine($"User Input : {x}\toutput is : {output} ");
+    }
+}
