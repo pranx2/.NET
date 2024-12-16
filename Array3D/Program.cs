aab457a149ef3885861290e530e3549d86fd8111
Pranto Kumar <prantokumar.net@gmail.com>
2024-12-17 00:24:51 +0600

    Array3D

---
+++ Array3D/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Array3D;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int[,,] array3D = { {{1,2,3}, {1,2,3}, {1,2,3}}, {{4,5,6},{4,5,6},{4,5,6}},{{7,8,9},{7,8,9},{7,8,9}}};
+        Console.WriteLine($"Array3D Showing...");
+        for(int i = 0;i < array3D.GetLength(0); i++ )
+        {
+            for(int j = 0; j < array3D.GetLength(1); j++)
+            {
+                for(int k = 0; k < array3D.GetLength(2); k++)
+                {
+                    Console.WriteLine($"Array3D [{i}x{j}x{k}] = {array3D[i,j,k]}");
+                }
+                Console.WriteLine($"");
+            }
+            Console.WriteLine($"");
+        }
+    }
+}
