c2226288a6e3d8abe5c0c36785d85356bb75c1df
Pranto Kumar <prantokumar.net@gmail.com>
2024-12-17 00:29:27 +0600

    all loops in array

---
+++ Array_Using_All_Loops/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Array_Using_All_Loops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int i = 0; // for point array index 0;
+
+        //using Do While Loop;
+        do{
+            Console.Write($"{num[i]} ");
+            i++;
+        }while(i < num.Length);
+
+        //line ghape;
+        Console.WriteLine($"");
+
+        //using While Loop;
+        int j = 0;
+    while(j <= num.Length){
+        Console.Write($"[{j}] , ");
+        j++;
+    }
+
+    //line ghape;
+    Console.WriteLine($"");
+    //using Foreach Loop;
+    foreach(int k in num){ // in foreach loop index start at index 1;
+        Console.Write($"[{k}] , ");
+    }
+
+    //line ghape;
+    Console.WriteLine($"");
+    //using For Loop;
+    for(int l = 0; l <= num.Length; l++){
+        Console.Write($"[{l}] , ");
+    }
+    }
+}
