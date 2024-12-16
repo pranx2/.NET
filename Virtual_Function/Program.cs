6ff689fe4c43773b12287abf5eb18eedf530e73e
Pranto Kumar <prantokumar.net@gmail.com>
2024-12-16 23:25:17 +0600

    Virtual keyWord

---
+++ Virtual_Function/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Virtual_Function;
+class car{ //! Base Class;
+    public virtual void Display(){
+        Console.WriteLine($"This is Virtual Class ! Car.......");
+    }
+}
+class Bike : car{ //Derive Class;
+    public override void Display()
+    {
+        Console.WriteLine($"This is Overridden Class ! Bike......");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        car c1 = new car();
+        c1.Display();
+
+        Bike b1 = new Bike();
+        b1.Display();
+    }
+}
+//! Virtual is just a way that same method but we can change it in the derived class;
